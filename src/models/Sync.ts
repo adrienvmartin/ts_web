95c76c8cbfd31d7c01af9b11268adfdf4b2f0c28
Adrien Martin <adrien.v.martin@gmail.com>
2021-04-06 16:46:56 -0600

    Return AxiosPromise

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosPromise, AxiosResponse } from 'axios';
+import axios, { AxiosPromise } from 'axios';
 import { UserProps } from './User';
 
 export class Sync {
@@ -8,13 +8,13 @@ export class Sync {
     return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): void {
+  save(data: UserProps): AxiosPromise {
     const { id } = data;
 
     if (id) {
-      axios.put(`${this.rootUrl}/${id}`, data);
+      return axios.put(`${this.rootUrl}/${id}`, data);
     } else {
-      axios.post(this.rootUrl, data);
+      return axios.post(this.rootUrl, data);
     }
   }
 }
