0095162f7d2ae70fa9fc88bd4f3c5d186d1156c5
Adrien Martin <adrien.v.martin@gmail.com>
2021-04-06 16:43:46 -0600

    Refactor variables to work with new methods

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -2,20 +2,21 @@ import axios, { AxiosResponse } from 'axios';
 import { UserProps } from './User';
 
 export class Sync {
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): void {
+    axios.get(`${this.rootUrl}/${id}`).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
   }
 
-  save(): void {
-    const id = this.get('id');
+  save(data: UserProps): void {
+    const { id } = data;
+
     if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
+      axios.put(`${this.rootUrl}/${id}`, data);
     } else {
-      axios.post('http://localhost:3000/users', this.data);
+      axios.post(this.rootUrl, data);
     }
   }
 }
