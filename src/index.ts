6650b14472606126c41eb95c33104235d37c9b24
Adrien Martin <adrien.v.martin@gmail.com>
2021-04-14 10:46:40 -0600

    Test new functionality

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
-user.on;
+console.log(user.get('name'));
+
+user.on('change', () => {
+  console.log('User was changed');
+});
+
+user.trigger('change');
