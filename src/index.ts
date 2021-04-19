f2695b2f37b79e7986c555e2d7a8139855d25da5
Adrien Martin <adrien.v.martin@gmail.com>
2021-04-19 10:26:13 -0600

    Test user creation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: 'Bob', age: 22 });
+
+const userForm = new UserForm(document.getElementById('root'), user);
 
 userForm.render();
