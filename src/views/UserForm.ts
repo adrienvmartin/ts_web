01a956d39f94c376eb5806d951da547459721f63
Adrien Martin <adrien.v.martin@gmail.com>
2021-04-19 10:20:58 -0600

    Add additional event handlers

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,11 +4,16 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover,
     };
   }
 
+  onHeaderHover(): void {
+    console.log('H1 was hovered over');
+  }
+
   onButtonClick(): void {
-    console.log('Hi there');
+    console.log('Button was clicked');
   }
 
   template(): string {
@@ -16,6 +21,7 @@ export class UserForm {
     <div>
       <h1>User Form</h1>
       <input />
+      <button>Click Me</button>
     </div>
     `;
   }
